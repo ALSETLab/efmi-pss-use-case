c881984533a2b3c76a929b2c37ece002222d425c
Luigi Vanfretti <luigi.vanfretti@gmail.com>
2025-10-25 12:34:55 -0400

    Removing typhoon generated target files

--- TyphoonHIL/20250917_TyphoonHIL_FMU_Tests/FMUimportTestSimplePiDymolaFMU Target files/ext-src/PSSDesign_FMUs4RTHIL_SimplePI/sources/fmiModelIdentifier.h
+++
@@ -1,7 +0,0 @@
-#ifndef FMI_MODELIDENTIFIER_H 
-#define FMI_MODELIDENTIFIER_H 
-#define MODEL_IDENTIFIER PSSDesign_FMUs4RTHIL_SimplePI 
-#ifndef __GNUC__ 
-#define FMI_MODULE_NAME PSSDesign_FMUs4RTHIL_SimplePI.dll 
-#endif 
-#endif 
